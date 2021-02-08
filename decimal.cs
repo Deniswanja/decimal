d7ddd5c3b239a825289280932f6b4b2b38b66b8e
Denis wanjala <61288857+Deniswanja@users.noreply.github.com>
2021-02-08 09:00:15 -0500

    Add files via upload

---
+++ decimal.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace AddFiveDecimalNumbers
+{
+    class Program
+    {
+        static float[] fivenumbers = { 1, 2, 3, 4, 5 };
+        static int count = 0;
+        static float value = 0;
+        static void Main(string[] args)
+        {
+            
+            while(count<fivenumbers.Length)
+            {
+                value += fivenumbers[count];
+                count += 1;
+            }
+            Console.WriteLine("Sum of five decimal numbers:{0}",value);
+        }
+    }
+}
